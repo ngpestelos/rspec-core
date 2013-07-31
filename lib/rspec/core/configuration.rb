f1e30bec0d0d2db94d1ad3b08b0e560972e3f45b
Jon Rowe <hello@jonrowe.co.uk>
2013-07-31 21:01:21 +1000

    remove double WARNING

--- lib/rspec/core/configuration.rb
+++ lib/rspec/core/configuration.rb
@@ -123,7 +123,7 @@ module RSpec
 
       def pattern= value
         if @spec_files_loaded
-          RSpec.warning "WARNING: Configuring `pattern` to #{value} has no effect since RSpec has already loaded the spec files."
+          RSpec.warning "Configuring `pattern` to #{value} has no effect since RSpec has already loaded the spec files."
         end
         @pattern = value
       end
